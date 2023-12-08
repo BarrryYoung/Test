1201d680852a8a827e4cfadcd1a4dd9b4c810738
BarrryYoung <47512477+BarrryYoung@users.noreply.github.com>
2023-12-08 10:40:24 +0000

    asdf

--- CommunicationFusion/shoulk.c
+++ CommunicationFusion/shoulk.c
@@ -218,7 +218,7 @@ int main() {
     printf("0:%02x\n",tap_frame[0]);
     printf("1:%02x\n",tap_frame[1]);
     printf("2:%02x\n",tap_frame[2]);
-    printf("%d",serial_frame_length-8);
+    printf("serial_frame_length:%d",serial_frame_length-8);
     for(int i=0;i<serial_frame_length-8;i++){
         printf(" %02x\n",tap_frame[i]);
     }
